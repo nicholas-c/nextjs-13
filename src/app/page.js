08a610129ad48e8e44fe3a66f57397e8b6998763
Nich Carter <nich@Nichs-MacBook-Pro.local>
2022-11-03 18:22:34 +0000

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,30 @@
 import Link from "next/link";
 
-const Page = () => {
+// async function getData() {
+//   const [races, lastRace] = await Promise.all([
+//     (
+//       await (await fetch("https://ergast.com/api/f1/2022.json")).json()
+//     ).MRData.RaceTable.Races,
+//     (
+//       await (
+//         await fetch("http://ergast.com/api/f1/current/last/results.json")
+//       ).json()
+//     ).MRData.RaceTable.Races[0],
+//   ]);
+
+//   const nextRace = races[Number(lastRace.round)];
+
+//   return {
+//     lastRace,
+//     nextRace,
+//   };
+// }
+
+const Page = async () => {
+  // const data = await getData();
+
+  // console.log(data);
+
   return (
     <>
       <div>This is my page</div>
